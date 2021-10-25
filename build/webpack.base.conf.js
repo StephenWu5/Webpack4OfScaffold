dc9d7a43792e21a65614260910bea420dea036de
Your Name <981079142@qq.com>
2021-10-25 21:04:21 +0800

    webpack打包处理

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ const path = require("path");
 //利用require导入了node中path模块,path模块了一些处理文件路径的小工具，由于webpack是基于Node环境下的工具，所以可以直接使用node原生模块path
 const SRC_PATH = path.resolve(__dirname, "../src");
 //path.resolve()方法可以将路径或者路径片段解析成绝对路径，具体可以结合官方文档进行查看,综合来看，path.resolve()是一个修改和整合文件路径的方法，dirname是directory+name的缩写，故名思义，是文件名的意思。总的来说就是将绝对路径提取出来
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
   entry: {
     main: "./src/index.js",
@@ -14,4 +15,36 @@ module.exports = {
     },
   },
   //Resolve 配置 Webpack 如何寻找模块所对应的文件,extensions:在导入语句没带文件后缀时，Webpack 会自动带上后缀后去尝试访问文件是否存在。alias: 配置项通过别名来把原导入路径映射成一个新的导入路径。
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, //通过loader来预处理文件 允许你打包除了js之外的任何静态资源
+        use: "vue-loader",
+      },
+      {
+        test: /\.js?$/,
+        use: "babel-loader",
+        //排除node_modules目录下的文件
+        exclude: /node_modules/,
+        include: SRC_PATH,
+      },
+      {
+        test: /\.(woff|svg|eot|woff2|tff)$/,
+        use: [
+          {
+            loader: "url-loader",
+            include: [resolve("src")],
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    //它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
+  ],
 };
---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,4 @@
+const path=require('path');
+const DIST_PATH=path.resolve(__dirname,"../dist");
+const CleanWebpackPlugin=require('clean-webpack-plugin');
+//打包之前清除文件
